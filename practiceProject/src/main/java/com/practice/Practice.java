53758a8363e535b994147cdfa3abe78a18b37342
Hana <hana.velecina@gmail.com>
2022-04-09 22:49:12 -0400

    Practice project

---
+++ practiceProject/src/main/java/com/practice/Practice.java
@@ -0,0 +1,41 @@
+package com.practice;
+
+public class Practice {
+	
+
+
+		
+		//we are doing arthimetic operation
+		
+		
+		
+		
+		public int multiplication(int a, int b) {
+			return a*b;
+			
+		}
+		
+		public double division(double a, double b) {
+			return a/b;
+		}
+		public int subtraction(int a, int b) {
+			return a-b;
+		}
+		
+		public double addition(double a, double b) {
+			return a+b;
+		}
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Practice c= new Practice();
+			
+			System.out.println(c.multiplication(12,13));
+			System.out.println(c.addition(12, 10));
+			System.out.println(c.subtraction(50, 26));
+			System.out.println(c.division(15, 2));
+		}
+
+	}
+	
+
+
